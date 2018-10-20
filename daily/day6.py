d5a87fdaf1abae472a9885a6ba3b52b28c28bf4f
Ravie403 <info@tkm.blue>
2018-10-20 10:47:09 +0900

    change day6

--- daily/day6.py
+++ daily/day6.py
@@ -1,32 +1,8 @@
-import urllib.request
-import json
-import collections
-
-
-def get_pokemon_data(n):
-    """
-
-    関数get_pokemon_dataはAPI(https://pokeapi.co/api/v2/)を利用することにより,
-    ずかん番号に対応するポケモンの情報を返す.
-
-    引数
-    n : 整数
-
-    返り値
-    data : 辞書
-
-    """
-    url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(n)
-    req = urllib.request.Request(url, headers={"User-Agent": "dummy"})
-    with urllib.request.urlopen(req) as f:
-        data = json.loads(f.read().decode("utf-8"))
-    return data
-
 
 if __name__ == "__main__":
     """
 
-    関数get_pokemon_dataを用いて,
+    day5で用いられた関数get_pokemon_dataを用いて,
 
     {
     'bulbasaur': 7, 'ivysaur': 10, 'venusaur': 20,
@@ -35,9 +11,11 @@ if __name__ == "__main__":
     }
 
     といったキャラクターの名前、高さの辞書を作成してください。
+    ただし、get_pokemon_dataはday5からimportしてください。
 
     さらに、_辞書内包表記_を用いて以下のような_高さが1.0m以下_の
-    ポケモンの名前、高さの辞書を1~30番までのポケモンで作成してください。
+    ポケモンの名前の辞書を1~30番までのポケモンで作成してください。
+    辞書の名前はchibiesにしてください。
 
     {
     'bulbasaur': 7, 'ivysaur': 10, 'charmander': 6,
@@ -45,6 +23,8 @@ if __name__ == "__main__":
     ...
     'sandshrew': 6, 'sandslash': 10, 'nidoran-f': 4
     }
-    
+
     0.1m = 1であることに注意してください。
     """
+
+    # print(chibies)

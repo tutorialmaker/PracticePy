d2290820b70daeea76bd168596414ae782b0d090
Hiroki Kojima <iamhirok@icloud.com>
2018-10-28 10:06:58 +0900

    Create day11

---
+++ daily/day11.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    """
+
+    Pythonにおけるすべてのクラスが持つ属性を除く,
+    以下に示す型の組み合わせに共通する属性名をそれぞれ出力せよ.
+    ただし, 属性が複数の場合, 出力結果はアルファベット順に従うこと.
+
+    (1) List, Tuple
+    (2) List, Tuple
+    (3) List, Tuple, Dict
+    (4) List, Tuple, Dict, str
+    (5) List, Tuple, Dict, str, Set
+
+    (5)の組み合わせに共通する属性について, 使用例を示せ.
+
+    """

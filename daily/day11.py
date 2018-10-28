f69a5b7188d6cfa0a94894d09806751422e4dcc8
Hiroki Kojima <iamhirok@icloud.com>
2018-10-28 10:18:52 +0900

    Update

--- daily/day11.py
+++ daily/day11.py
@@ -3,14 +3,13 @@ if __name__ == "__main__":
 
     Pythonにおけるすべてのクラスが持つ属性を除く,
     以下に示す型の組み合わせに共通する属性名をそれぞれ出力せよ.
-    ただし, 属性が複数の場合, 出力結果はアルファベット順に従うこと.
 
     (1) List, Tuple
-    (2) List, Tuple
-    (3) List, Tuple, Dict
-    (4) List, Tuple, Dict, str
-    (5) List, Tuple, Dict, str, Set
+    (2) List, Tuple, Dict
+    (3) List, Tuple, Dict, str
+    (4) List, Tuple, Dict, str, Set
 
-    (5)の組み合わせに共通する属性について, 使用例を示せ.
+    また(2), (3), (4)の組み合わせに共通する属性について,
+    それぞれDict, str, Setを用いて使用例を示せ.
 
     """

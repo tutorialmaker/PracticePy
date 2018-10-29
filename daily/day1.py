d63ce4b10910cf91e33881d2dbd84acf22097694
hiroki <iamhirok@icloud.com>
2018-10-29 14:00:43 +0900

    Revert "day3"

--- daily/day1.py
+++ daily/day1.py
@@ -13,26 +13,26 @@ if __name__ == "__main__":
     float
     int
     str
-    
+
     または関数(引数は各自で設定する)
-    
+
     type()
 
     を, objectの代わりにtype_1からtype_5に一回づつ代入し,
     (1)から(5)の出力結果をすべてTrueにせよ.
 
     """
-    type_1 = object
+    type_1 = int
     print(isinstance(0, type_1)) # (1)
 
-    type_2 = object
+    type_2 = float
     print(isinstance(0., type_2)) # (2)
 
-    type_3 = object
+    type_3 = str
     print(isinstance("0", type_3)) # (3)
 
-    type_4 = object
+    type_4 = bool
     print(isinstance(False, type_4)) # (4)
 
-    type_5 = object
+    type_5 = type(None)
     print(isinstance(empty(), type_5)) # (5)

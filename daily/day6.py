d6897577ae2eb3e6c778a19b386099804abb7914
Hiroki Kojima <iamhirok@icloud.com>
2018-11-19 06:34:28 +0900

    day6を変更

--- daily/day6.py
+++ daily/day6.py
@@ -1,40 +1,9 @@
-
 if __name__ == "__main__":
     """
 
-    day5で用いられた関数get_pokemon_dataを用いて,
+    day5で作成した順序付き辞書に対して,
+    存在しないキー(例えば"MissingNo.")をキーとしてバリューの取得を試みた場合,
+    KeyErrorが発生する.
+    存在しないキーを指定した場合, デフォルトとしてNoneを返すようなバリューの取得方法を示せ.
 
-    {
-        'bulbasaur': 7,
-        'ivysaur': 10,
-        'venusaur': 20,
-        'charmander': 6,
-        'charmeleon': 11,
-        'charizard': 17,
-        'squirtle': 5,
-        'wartortle': 10,
-        'blastoise': 16
-    }
-
-    といったポケモンの名前,高さの辞書を作成してください。
-    ポケモンの名前はnameキー、高さはheightキーで取得できます。
-    ただし、get_pokemon_dataはday5からimportしてください。
-
-    さらに、_辞書内包表記_を用いて以下のような_高さが1.0m以下_の
-    ポケモンの名前,高さの辞書を1~30番までのポケモンで作成してください。
-    辞書の名前はchibiesにしてください。
-
-    {
-        'bulbasaur': 7,
-        'ivysaur': 10,
-        'charmander': 6,
-        ...
-        'sandshrew': 6,
-        'sandslash': 10,
-        'nidoran-f': 4
-    }
-
-    0.1m = 1であることに注意してください。
     """
-
-    # print(chibies)

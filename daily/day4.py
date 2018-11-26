4a135c0c04d97449e8375877a43e44da5eb99800
ebakazu <ebara.kazuma@gmail.com>
2018-11-26 21:37:37 +0900

    day4の実施

---
+++ daily/day4.py
@@ -0,0 +1,42 @@
+import urllib.request
+import urllib.error
+import json
+
+def kanto_pokedex():
+    """
+
+    関数kanto_pokedexはAPI(https://pokeapi.co/api/v2/)を利用することにより,
+    ずかん番号1から151に対応するポケモンの情報を返す.
+
+    返り値
+    pokemons : List[Any]
+
+    """
+
+    url = "https://pokeapi.co/api/v2/pokedex/2"
+    req = urllib.request.Request(url, headers={"User-Agent": "dummy"})
+    try:
+        with urllib.request.urlopen(req) as f:
+            pokemons = json.loads(f.read().decode("utf-8"))["pokemon_entries"]
+        return pokemons
+    except urllib.error.URLError:
+        print("\nError: The requested URL could not be retrived. \
+                        Are you connected to the Internet?\n")
+
+
+if __name__ == "__main__":
+    """
+
+    関数kanto_pokedexを用いて,
+    ずかん番号1から151に対応するポケモンの名称をリストpokemon_nameに格納せよ.
+
+    """
+    pokemons = kanto_pokedex()
+    pokemon_name = {}
+    for poke in pokemons:
+        key = poke['entry_number']
+        specie = poke['pokemon_species']
+        value = specie['name']
+        pokemon_name[key] = value
+
+    print(pokemon_name)

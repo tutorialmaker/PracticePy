7e448e16dae2fe45909ce251de341d93eb32182b
saikishunsuke <saikishunsuke0828@yahoo.co.jp>
2018-12-01 00:48:21 +0900

    day5の更新

--- daily/day5.py
+++ daily/day5.py
@@ -1,50 +1,36 @@
 import urllib.request
+import urllib.error
 import json
-import collections
 
 
 def get_pokemon_data(n):
     """
-
     関数get_pokemon_dataはAPI(https://pokeapi.co/api/v2/)を利用することにより,
     ずかん番号に対応するポケモンの情報を返す.
-
     引数
-    n : 整数
-
+    n : int
     返り値
-    data : 辞書
-
+    data : Dict[Any, Any]
     """
-    url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(n)
+    url = f'https://pokeapi.co/api/v2/pokemon/{n}/'
     req = urllib.request.Request(url, headers={"User-Agent": "dummy"})
-    with urllib.request.urlopen(req) as f:
-        data = json.loads(f.read().decode("utf-8"))
-
-    return data
+    try:
+        with urllib.request.urlopen(req) as f:
+            data = json.loads(f.read().decode("utf-8"))
+        return data
+    except urllib.error.URLError:
+        print("\nError: The requested URL could not be retrived. \
+                        Are you connected to the Internet?\n")
 
 
 if __name__ == "__main__":
     """
-
     関数get_pokemon_dataを用いて,
-    以下に示すような, キーにポケモンの名称、バリューにそのポケモンの重量を持つ辞書を作成せよ.
-
-    {
-        'bulbasaur': 69, 'ivysaur': 130, 'venusaur': 1000,
-        'charmander': 85, 'charmeleon': 190, 'charizard': 905,
-        'squirtle': 90, 'wartortle': 225, 'blastoise': 855
-    }
-
-    さらに以下に示すような, 重量が重い順に並んだ順序付き辞書を作成せよ.
-
-    OrderedDict([
-        ('venusaur', 1000), ('charizard', 905), ('blastoise', 855),
-        ('wartortle', 225), ('charmeleon', 190), ('ivysaur', 130),
-        ('squirtle', 90), ('charmander', 85), ('bulbasaur', 69)
-        ])
-
-    ただしポケモンの名称および重量は, 関数get_pokemon_dataの返り値において,
-    バリューとしてキー"name"および"weight"に対応している.
-
+    キーにポケモンの名称, バリューにそのポケモンのBMIを持つ辞書を作成せよ.
+    またBMIが高い順に並んだ順序付き辞書を作成せよ.
+    また, 引数にしきい値とBMIが高い順に並んだ順序付き辞書をとり,
+    しきい値を下回るBMIのポケモンのみによる順序付き辞書を返す関数を定義して実行せよ.
+    ただしポケモンの名称および重量および高さは, 関数get_pokemon_dataの返り値において,
+    バリューとしてキー"name"および"weight"および"height"に対応しており,
+    "weight"および"height"の値の単位はそれぞれ0.1メートルおよび0.1キログラムである.
     """

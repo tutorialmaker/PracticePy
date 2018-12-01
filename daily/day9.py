dc53cec81b83b435688296fad718002dd623de96
Hiroki Kojima <iamhirok@icloud.com>
2018-12-02 00:28:33 +0900

    Revert "オブジェクトのサイズについて言及"
    This reverts commit a38558658e3d788db15f765cef427851f418df12.

--- daily/day9.py
+++ daily/day9.py
@@ -25,16 +25,15 @@ if __name__ == '__main__':
     # inveted_map_3はリストなので遅延評価ではない
     print(inverted_map_3)
 
-    # inverted_map_4はイテレータなので遅延評価
+    # inverted_map_4は遅延評価
     print(list(inverted_map_4))
 
     # mapは元の配列に変化は起こしません
     print(list(nums))
 
-    # TODO: 30~100までの数字の配列(nums_2)を作り
-    # 上記1, 2, 3, 4の方法にならい、それぞれ100を引いてみてください
-    # また3と4の方法について, それぞれのオブジェクトのサイズを示してください
-    # ただしnums_2の中身は変更しないようにしてください
+    # TODO: 30~100までの数字の配列(num2)を作り
+    # 上記1, 2, 3, 4の方法に習い、それぞれ100を引いてみてください
+    # ただしnum2の中身は変更しないようにしてください。
 
     # nums_2 =
     # print(list(nums))
@@ -42,14 +41,10 @@ if __name__ == '__main__':
     # hundredfold_map_1 =
     # hundredfold_map_2 =
     # hundredfold_map_3 =
-    # sizeof3 =
     # hundredfold_map_4 =
-    # sizeof4 =
 
     # print(list(hundredfold_map_1))
     # print(list(hundredfold_map_2))
     # print(hundredfold_map_3)
-    # print(f"hundredfold_map_3 : {sizeof3} bytes")
     # print(list(hundredfold_map_4))
-    # print(f"hundredfold_map_4 : {sizeof4} bytes")
-    # print(nums_2)
+    # print(num_2)

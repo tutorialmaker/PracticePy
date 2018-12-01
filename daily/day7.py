f16ff1680a3d00649652125ac964da37a7f06628
saikishunsuke <saikishunsuke0828@yahoo.co.jp>
2018-12-02 03:57:42 +0900

    day7の実施

--- daily/day7.py
+++ daily/day7.py
@@ -1,22 +1,24 @@
+from day5 import get_pokemon_data
+
 class Pokedex:
     def __init__(self):
         self._pokemons = {}
 
     @property
-    def pokemon(self):
-        pass
+    def pokemons(self):
+        return self._pokemons
 
-    @pokemon.getter
-    def pokemon(self):
-        pass
+    @pokemons.getter
+    def pokemons(self):
+        return self._pokemons
 
-    @pokemon.setter
-    def pokemon(self):
-        pass
+    @pokemons.setter
+    def pokemons(self, data):
+        self._pokemons = {data['id']: data}
 
-    @pokemon.deleter
-    def pokemon(self):
-        pass
+    @pokemons.deleter
+    def pokemons(self):
+        del self._pokemons
 
 
 if __name__ == "__main__":
@@ -38,3 +40,17 @@ if __name__ == "__main__":
     # キーがnumberのオブジェクトを削除する.
     del mypkd.pokemons[number]
     """
+
+    # Pokedexのインスタンスを生成する.
+    mypkd = Pokedex()
+    # numberはポケモンずかんにおけるポケモンの番号を表す.
+    number = 10
+    # mypkdのセッターを用いて, mypkdのインスタンス変数_pokemonsに,
+    # バリューにget_pokemon_dataの返り値を持ち, キーをずかん番号とするオブジェクトを設定する.
+    mypkd.pokemons = get_pokemon_data(number)
+    # mypkdのゲッターを用いて, _pokemonsを取得する.
+    pokemons = mypkd.pokemons
+    # 取得した辞書からずかん番号numberのポケモンの名称を出力する.
+    print(pokemons[number]["name"])
+    # キーがnumberのオブジェクトを削除する.
+    del mypkd.pokemons[number]

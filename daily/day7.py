5b9f99219c6cbbc796af5aa15cdc439d56cf70a4
Hiroki Kojima <iamhirok@icloud.com>
2018-12-02 10:28:03 +0900

    問題文を明示的な指示に修正

--- daily/day7.py
+++ daily/day7.py
@@ -29,20 +29,24 @@ if __name__ == "__main__":
     # Pokedexのインスタンスを生成する.
     mypkd = Pokedex()
 
-    # numberはポケモンずかんにおけるポケモンの番号を表す.
-    number = 10
+    # n10およびn20はポケモンずかんにおけるポケモンの番号を表す.
+    n10, n20 = 10, 20
 
     # mypkdのセッターを用いて, mypkdのインスタンス変数_pokemonsに,
     # バリューにget_pokemon_dataの返り値を持ち, キーをずかん番号とするオブジェクトを設定する.
-    mypkd.pokemons = get_pokemon_data(number)
+    mypkd.pokemons = get_pokemon_data(n10)
+    mypkd.pokemons = get_pokemon_data(n20)
 
     # mypkdのゲッターを用いて, _pokemonsを取得する.
     pokemons = mypkd.pokemons
 
-    # 取得した辞書からずかん番号numberのポケモンの名称を出力する.
-    print(pokemons[number]["name"])
+    # 取得した辞書からずかん番号10のポケモンの名称を出力する.
+    print(pokemons[n10]["name"])
 
-    # キーがnumberのオブジェクトを削除する.
-    del mypkd.pokemons[number]
+    # キーがn10のオブジェクトを削除する.
+    del mypkd.pokemons[n10]
+
+    # 取得した辞書からずかん番号20のポケモンの名称を出力する.
+    print(pokemons[n20]["name"])
 
     """

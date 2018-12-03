c63c27d6e1b6730ed36048d26110c89623ed69a4
Hiroki Kojima <iamhirok@icloud.com>
2018-12-03 18:16:36 +0900

    day16を作成

---
+++ daily/day16.py
@@ -0,0 +1,73 @@
+"""
+
+    TODOを実装せよ.
+
+"""
+from numbers import Number
+from reprlib import recursive_repr
+
+
+class Matrix(list):
+    __slots__ = ["_shape", ]
+
+    def __init__(self, M):
+        super().__init__(M)
+
+        rlens = []
+        for row in self:
+            if not hasattr(row, "__iter__"):
+                raise TypeError(f"{type(row)} object is not iterable")
+
+            rlens.append(len(row))
+            for v in row:
+                if not isinstance(v, Number):
+                    raise TypeError(
+                        f"Matrix element must be a Number, not {type(v)}")
+
+        rlen = max(rlens)
+        if rlen != min(rlens):
+            raise ValueError(f"unexpected Matrix shape")
+        clen = len(rlens)
+        self._shape = (clen, rlen)
+
+    def __mul__(self, other):
+        # TODO: 行列積の計算を実装
+        return NotImplemented
+
+    def getshape(self):
+        return self._shape
+
+    def getT(self):
+        # TODO: 転置を返すメソッドを実装
+        pass
+
+    @recursive_repr()
+    def __repr__(self):
+        return f"{self.__class__.__name__}({list(self)})"
+
+    shape = property(getshape, None)
+    # TODO: 転置について読み取り専用のアクセサを実装
+
+
+if __name__ == "__main__":
+    A = [[1, 2, 3],
+         [4, 5, 6]]
+
+    B = [[7],
+         [8],
+         [9]]
+
+    try:
+        import numpy as np
+        """
+
+            Numpyを用いてAとBの内積を求めよ.
+
+        """
+
+    except ImportError:
+        """
+
+            定義したMatrixを用いてAとBの内積を求めよ.
+
+        """

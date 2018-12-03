e289825d6b3a2a67a51cf555cd31703cc2139861
Hiroki Kojima <iamhirok@icloud.com>
2018-12-04 01:44:32 +0900

    updateを削除

--- daily/day14.py
+++ daily/day14.py
@@ -21,16 +21,6 @@
         min.set(HP=200, ATK=300, DEF=100)
         don.set(HP=100, ATK=100, DEF=50)
 
-        print("\n///Before Buttle.///\n")
-        book.show()
-
-        mic.update(dH=10, dA=5, dD=10)
-        min.update(dH=5, dA=10, dD=10)
-        don.update(dH=50, dA=5, dD=5)
-
-        print("\n///After Buttle.///\n")
-        book.show()
-
 """
 
 
@@ -90,37 +80,6 @@ class SpeciesBook:
             "DEF" : DEF,
         }
 
-    def update(self, species, name, *, dH, dA, dD):
-        """追加済みのモンスターのステータスを更新する
-
-        Parameters
-        ----------
-
-        species : str
-            既に追加した種族の名称.
-
-        name : str
-            既に追加したモンスターの名称.
-
-        dH : int
-            体力の差分.
-
-        dA : int
-            攻撃力の差分.
-
-        dD : int
-            守備力の差分.
-
-        """
-        if species not in self._status:
-            raise Exception(f"Dont have {species} yet!")
-        if name not in self._status[species]:
-            raise Exception(f"Dont have {name} yet!")
-
-        self._status[species][name]["HP"] += dH
-        self._status[species][name]["ATK"] += dA
-        self._status[species][name]["DEF"] += dD
-
     def show(self):
         print("*@"*8, "STATUS", "*@"*8)
         for species in self._status:
@@ -145,12 +104,4 @@ if __name__ == "__main__":
     book.set_species("Duck")
     book.set_monster("Duck", "Don", HP=100, ATK=100, DEF=50)
 
-    print("\n///Before Buttle.///\n")
-    book.show()
-
-    book.update("Mouse", "Mic", dH=10, dA=5, dD=10)
-    book.update("Mouse", "Min", dH=5, dA=10, dD=10)
-    book.update("Duck", "Don", dH=50, dA=5, dD=5)
-
-    print("\n///After Buttle.///\n")
     book.show()

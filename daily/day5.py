1c61cf272aebdb8783e53f554c47a94f7d5f9684
YosukeHoshi <yosuke.star.520@gmail.com>
2018-12-04 01:17:43 +0900

    変数名の変更と二重定義の解消

--- daily/day5.py
+++ daily/day5.py
@@ -1,6 +1,6 @@
 import urllib.request
 import json
-import collections
+from collections import OrderedDict
 
 
 def get_pokemon_data(n):
@@ -39,28 +39,27 @@ if __name__ == "__main__":
     "weight"および"height"の値の単位はそれぞれ0.1メートルおよび0.1キログラムである.
 
     """
-#keyにポケモンの名前valueにBMIをもつ辞書の作成
-pokemon_dict = {}
+# keyにポケモンの名前valueにBMIをもつ辞書の作成
+dic = {}
 for i in range(3):
-    pokemon = get_pokemon_data(i+1)
-    pokemon_name = pokemon["name"]
-    pokemon_bmi = (pokemon["weight"]/(pokemon["height"])**2)*10
-    pokemon_dict[pokemon_name] = pokemon_bmi
-print(pokemon_dict)
+    data = get_pokemon_data(i+1)
+    name = data["name"]
+    bmi = (data["weight"]/(data["height"])**2)*10
+    dic[name] = bmi
+print(dic)
 
-#BMIの高い順に並び替え
-dict_sorted = {}
+# BMIの高い順に並び替え
 dict_sorted = OrderedDict()
-for k, v in sorted(pokemon_dict.items(), key=lambda x: x[1], reverse=True):
+for k, v in sorted(dic.items(), key=lambda x: x[1], reverse=True):
     dict_sorted[k] = v
 print(dict_sorted)
 
-#引数にしきい値とBMIが高い順に並んだ順序付き辞書をとって閾値以下のBMIのポケモンのみの辞書を返す
-def threshold(x,dict_sorted):
+# 引数にしきい値とBMIが高い順に並んだ順序付き辞書をとって閾値以下のBMIのポケモンのみの辞書を返す
+def threshold(x, dict_sorted):
     under_threshold = {}
     for k, v in dict_sorted.items():
         if v < x:
             under_threshold[k] = v
-    under_threshold = dict(sorted(under_threshold.items(), key=lambda x:x[1], reverse=True))
+    under_threshold = dict(sorted(under_threshold.items(), key=lambda x: x[1], reverse=True))
     return under_threshold
-print(threshold(15,dict_sorted))
+print(threshold(15, dict_sorted))

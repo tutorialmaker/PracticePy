d980c29a9d853a32b18973bcc411020a8367126b
YosukeHoshi <yosuke.star.520@gmail.com>
2018-12-04 00:42:53 +0900

    fix for day5

--- daily/day5.py
+++ daily/day5.py
@@ -31,12 +31,15 @@ if __name__ == "__main__":
     キーにポケモンの名称、バリューにそのポケモンのBMIを持つ辞書を作成せよ.
     またBMIが高い順に並んだ順序付き辞書を作成せよ.
 
+    また, 引数にしきい値とBMIが高い順に並んだ順序付き辞書をとり,
+    しきい値を下回るBMIのポケモンのみによる順序付き辞書を返す関数を定義して実行せよ.
+
     ただしポケモンの名称および重量および高さは, 関数get_pokemon_dataの返り値において,
     バリューとしてキー"name"および"weight"および"height"に対応しており,
     "weight"および"height"の値の単位はそれぞれ0.1メートルおよび0.1キログラムである.
 
     """
-
+#keyにポケモンの名前valueにBMIをもつ辞書の作成
 pokemon_dict = {}
 for i in range(3):
     pokemon = get_pokemon_data(i+1)
@@ -45,23 +48,19 @@ for i in range(3):
     pokemon_dict[pokemon_name] = pokemon_bmi
 print(pokemon_dict)
 
+#BMIの高い順に並び替え
 dict_sorted = {}
+dict_sorted = OrderedDict()
 for k, v in sorted(pokemon_dict.items(), key=lambda x: x[1], reverse=True):
     dict_sorted[k] = v
 print(dict_sorted)
 
-def under_threshold(x):
-    dict_under_threshold = {}
-    for i in range(1,5):
-        data = get_pokemon_data(i)
-        name = data["name"]
-        weight = data["weight"]
-        height = data["height"]
-        BMI = weight/height**2
-        bmi = BMI*10
-        if bmi < x:
-            dict_under_threshold[name] = bmi
-            dict_sort = {}
-            for k, v in sorted(dict_under_threshold.items(), key=lambda x: x[1], reverse=True):
-                dict_sort[k] = v
-    return dict_sort
+#引数にしきい値とBMIが高い順に並んだ順序付き辞書をとって閾値以下のBMIのポケモンのみの辞書を返す
+def threshold(x,dict_sorted):
+    under_threshold = {}
+    for k, v in dict_sorted.items():
+        if v < x:
+            under_threshold[k] = v
+    under_threshold = dict(sorted(under_threshold.items(), key=lambda x:x[1], reverse=True))
+    return under_threshold
+print(threshold(15,dict_sorted))

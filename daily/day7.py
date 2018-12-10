d1e9db265cc1a9da851782720170749a9ce8d273
takuto0212 <tbugmt@icloud.com>
2018-12-10 20:10:42 +0900

    day7の実施

--- daily/day7.py
+++ daily/day7.py
@@ -1,46 +1,40 @@
-import urllib.request
-import json
-import collections
+from day5 import get_pokemon_data
 
-def get_pokemon_data_sp(n):
-    """
+class Pokedex:
+    def __init__(self):
+        self._pokemons = {}
 
-    関数get_pokemon_data_spはAPI(https://pokeapi.co/api/v2/)を利用することにより,
-    ずかん番号に対応するポケモンの図鑑情報を返す.
+    @property
+    def pokemons(self):
+        return self._pokemons
 
-    引数
-    n : 整数
+    @pokemons.getter
+    def pokemons(self):
+        return self._pokemons
 
-    返り値
-    data : 辞書
+    @pokemons.setter
+    def pokemons(self, data):
+        self._pokemons[data["id"]] = data
 
-    """
-    url = 'https://pokeapi.co/api/v2/pokemon-species/{}/'.format(n)
-    req = urllib.request.Request(url, headers={"User-Agent": "dummy"})
-    with urllib.request.urlopen(req) as f:
-        data = json.loads(f.read().decode("utf-8"))
-
-    return data
-
+    @pokemons.deleter
+    def pokemons(self):
+        del self._pokemons
 
 if __name__ == "__main__":
-    """
-
-    関数get_pokemon_data_spをコンストラクタに用い、
-    ポケモンの分類(genera)、名前(names)に関するクラスPokemonI18nを作成してください。
-    このクラスでは引数に言語コードをとり、その言語コードにおけるポケモンの分類、名前を返すメソッドを定義してください。
-    メソッド名は分類->group、名前->nameとしてください。
-    ただし、引数の言語コードは、APIから取得できる言語しか与えられないことを想定して構いません。
-
-    また、図鑑番号1~15の範囲で、図鑑番号に対応した情報を返す辞書pokemon_infoを作成してください。
-
-    この課題ではAPIから取得されるデータのキーに関する情報は最低限しか与えられません。
-    まずはhttps://pokeapi.co/api/v2/pokemon-species/1
-    のデータを取得して、どのようなデータがあるのかを確かめてください。
-
-    """
-
-    # print(pokemon_info[1].name('en')) # Bulbasaur
-    # print(pokemon_info[1].group('de')) # Samen
-    # print(pokemon_info[2].name('roomaji')) # Fushigisou
-    # print(pokemon_info[2].group('fr')) # Pokémon Graine
+    
+    # Pokedexのインスタンスを生成する.
+    mypkd = Pokedex()
+    # n10およびn20はポケモンずかんにおけるポケモンの番号を表す.
+    n10, n20 = 10, 20
+    # mypkdのセッターを用いて, mypkdのインスタンス変数_pokemonsに,
+    # バリューにget_pokemon_dataの返り値を持ち, キーをずかん番号とするオブジェクトを設定する.
+    mypkd.pokemons = get_pokemon_data(n10)
+    mypkd.pokemons = get_pokemon_data(n20)
+    # mypkdのゲッターを用いて, _pokemonsを取得する.
+    pokemons = mypkd.pokemons
+    # 取得した辞書からずかん番号10のポケモンの名称を出力する.
+    print(pokemons[n10]["name"])
+    # キーがn10のオブジェクトを削除する.
+    del mypkd.pokemons[n10]
+    # 取得した辞書からずかん番号20のポケモンの名称を出力する.
+    print(pokemons[n20]["name"])

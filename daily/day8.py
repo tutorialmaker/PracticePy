9d0495dec9fb11494a06598cd786edc90cb3ddc5
takuto0212 <tbugmt@icloud.com>
2018-12-17 20:55:18 +0900

    day8

--- daily/day8.py
+++ daily/day8.py
@@ -1,21 +1,21 @@
 import random
 
+
 def to_str(byte):
     return byte.decode()
+
 
 def o_or_x(fitzpatrick_scale):
     uni = [0x1F645, 0x1F646]
     color = [0x1F3FB, 0x1F3FC, 0x1F3FD, 0x1F3FE, 0x1F3FF]
-
-    if　not fitzpatrick_scale in range(6):
-        raise ValueError("The value input is invaild .")
-    elif fitzpatrick_scale == 0:
-        str_pic = chr(random.choice(uni))
+    str_pic = chr(random.choice(uni))
+    if fitzpatrick_scale == 0:
+        return str_pic
+    elif fitzpatrick_scale in range(1, 6):
+        str_pic_color = str_pic + chr(color[fitzpatrick_scale-1])
+        return str_pic_color
     else:
-        str_pic = chr(random.choice(uni)) + chr(color[fitzpatrick_scale-1])
-
-    return str_pic
-
+        raise ValueError("The value input is invalid .")
 
 
 if __name__ == "__main__":
@@ -29,5 +29,5 @@ if __name__ == "__main__":
     ただしfitzpatrick_scaleに0から5以外の値が与えられた場合, エラーを出力せよ.
     """
     print(to_str(b"\xf0\x9f\x91\x8d"))
-    for i in range(7):
-        print(o_or_x(i))
+    i = int(input("0~5の値を入力してください"))
+    print(o_or_x(i))

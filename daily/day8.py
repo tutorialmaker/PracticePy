9ae75f8c26e98c9ca7c19f79f6f5b3aec8205e93
ebakazu <ebara.kazuma@gmail.com>
2018-12-18 17:59:02 +0900

    day8

--- daily/day8.py
+++ daily/day8.py
@@ -1,3 +1,58 @@
+import random
+
+def to_str(bytes):
+    str = bytes.decode('utf-8')
+    return str
+
+def o_or_x():
+    num = random.randrange(1)
+    if num == 1:
+        return '\U0001F645'
+    else:
+        return '\U0001F646'
+
+def change_color(num):
+    if num == 0:
+        color = None
+    elif num == 1:
+        color = '\U0001F3FB'
+    elif num == 2:
+        color = '\U0001F3FC'
+    elif num == 3:
+        color = '\U0001F3FD'
+    elif num == 4:
+        color = '\U0001F3FE'
+    elif num == 5:
+        color = '\U0001F3FF'
+    return color
+
+def num_checker(num):
+    flag = 0
+    while flag == 0:
+        if num < 0 or num > 5:
+            print('入力された値は%dです.指定された範囲を超えています.' % num)
+            print('0~5の範囲の整数を入力してください.')
+            num = int(input())
+        else:
+            flag = 1
+    return num
+
+def main():
+    str = to_str(b'\xf0\x9f\x91\x8d')
+    print(str)
+    print('0~5の範囲の整数を入力してください.')
+    fitzpatrick_scale = int(input())
+
+    fitzpatrick_scale = num_checker(fitzpatrick_scale)
+    fitzpatrick_scale = change_color(fitzpatrick_scale)
+
+    codepoint = o_or_x()
+    codepoint = codepoint + fitzpatrick_scale
+    codepoint = codepoint.encode()
+    print(to_str(codepoint))
+
+
+
 if __name__ == "__main__":
     """
 
@@ -14,3 +69,4 @@ if __name__ == "__main__":
 
 
     """
+    main()

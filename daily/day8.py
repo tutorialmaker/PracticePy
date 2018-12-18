0b820970638794903f76e5789c333316bc2307ab
ebakazu <ebara.kazuma@gmail.com>
2018-12-18 19:27:53 +0900

    fix day8

--- daily/day8.py
+++ daily/day8.py
@@ -1,41 +1,39 @@
 import random
 
+
 def to_str(bytes):
     str = bytes.decode('utf-8')
     return str
 
-def o_or_x():
+
+def o_or_x(fitzpatrick_scale):
+    fitzpatrick_scale = num_checker(fitzpatrick_scale)
+    color = change_color(fitzpatrick_scale)
     num = random.randrange(2)
     if num == 1:
-        return '\U0001F645'
+        codepoint = '\U0001F645'
     else:
-        return '\U0001F646'
+        codepoint = '\U0001F646'
+    if color is None:
+        codepoint = codepoint
+    else:
+        codepoint = codepoint + color
+    codepoint = codepoint.encode()
+    return codepoint
+
 
 def change_color(num):
-    if num == 0:
-        color = None
-    elif num == 1:
-        color = '\U0001F3FB'
-    elif num == 2:
-        color = '\U0001F3FC'
-    elif num == 3:
-        color = '\U0001F3FD'
-    elif num == 4:
-        color = '\U0001F3FE'
-    elif num == 5:
-        color = '\U0001F3FF'
+    list = [None, '\U0001F3FB', '\U0001F3FC',
+            '\U0001F3FD', '\U0001F3FE', '\U0001F3FF']
+    color = list[num]
     return color
 
+
 def num_checker(num):
-    flag = 0
-    while flag == 0:
-        if num < 0 or num > 5:
-            print('入力された値は%dです.指定された範囲を超えています.' % num)
-            print('0~5の範囲の整数を入力してください.')
-            num = int(input())
-        else:
-            flag = 1
+    if num < 0 or num > 5:
+        raise ValueError('入力された値は,指定された範囲を超えています.')
     return num
+
 
 def main():
     str = to_str(b'\xf0\x9f\x91\x8d')
@@ -43,14 +41,8 @@ def main():
     print('0~5の範囲の整数を入力してください.')
     fitzpatrick_scale = int(input())
 
-    fitzpatrick_scale = num_checker(fitzpatrick_scale)
-    fitzpatrick_scale = change_color(fitzpatrick_scale)
-
-    codepoint = o_or_x()
-    codepoint = codepoint + fitzpatrick_scale
-    codepoint = codepoint.encode()
+    codepoint = o_or_x(fitzpatrick_scale)
     print(to_str(codepoint))
-
 
 
 if __name__ == "__main__":
